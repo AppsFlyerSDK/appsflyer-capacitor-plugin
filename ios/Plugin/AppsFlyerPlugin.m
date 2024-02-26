f2cb9a0c5f41730cf2ff0b27e9d27b985f4e791b
Dani Kozachkevich <Dani.ko@appsflyer.com>
2024-02-26 19:31:50 +0200

    fixed according to code review

--- ios/Plugin/AppsFlyerPlugin.m
+++ ios/Plugin/AppsFlyerPlugin.m
@@ -38,7 +38,7 @@ CAP_PLUGIN(AppsFlyerPlugin, "AppsFlyerPlugin",
            CAP_PLUGIN_METHOD(setPartnerData, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(logInvite, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(setSharingFilterForPartners, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(enableTCFDataCollection, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(enableTCFDataCollection, CAPPluginReturnNone);
            CAP_PLUGIN_METHOD(setConsentData, CAPPluginReturnNone);
            CAP_PLUGIN_METHOD(startSDK, CAPPluginReturnPromise);
 
