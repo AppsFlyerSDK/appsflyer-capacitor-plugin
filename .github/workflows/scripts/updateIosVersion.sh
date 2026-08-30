8e5f76109076a49420e963f2b47a00cf8a870ba6
Amit Levy <amit.levy@appsflyer.com>
2026-08-30 14:49:33 +0300

    feat(ci): add ios_framework_version input and validate AppsFlyerRPC/AppsFlyerFramework pin pairing
    AppsFlyerFramework-Static was previously a hand-maintained pin in Package.swift with no workflow_dispatch input, unlike Android's plugin-bridge version. Also stage the iosFrameworkVersion field in package.json with: git add -p package.json

--- .github/workflows/scripts/updateIosVersion.sh
+++ .github/workflows/scripts/updateIosVersion.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
 # Check that there are two arguments
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 newIosVersion"
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 newIosVersion newIosFrameworkVersion"
   exit 1
 fi
 
 # Assign arguments to variables
 newIosVersion=$1
+newIosFrameworkVersion=$2
 
 # Output the arguments
 echo "pwd: $(pwd)"
-echo "New iOS SDK Version: $newIosVersion"
+echo "New iOS SDK (AppsFlyerRPC) Version: $newIosVersion"
+echo "New iOS AppsFlyerFramework-Static Version: $newIosFrameworkVersion"
 echo "Updating Package.json file"
 sed -i -r -e "s/\"iosSdkVersion\": \"[0-9]+.[0-9]+.[0-9]+(-rc[0-9]+)?\"/\"iosSdkVersion\": \"$newIosVersion\"/gi" package.json
+sed -i -r -e "s/\"iosFrameworkVersion\": \"[0-9]+.[0-9]+.[0-9]+(-rc[0-9]+)?\"/\"iosFrameworkVersion\": \"$newIosFrameworkVersion\"/gi" package.json
 echo "Updating README.md file"
 sed -i -r -e "s/iOS AppsFlyer SDK \*\*[0-9]+.[0-9]+.[0-9]+(-rc[0-9]+)?\*\*/\iOS AppsFlyer SDK **$newIosVersion**/gi" README.md
-echo "Updating Package.swift SwiftPM pin"
+echo "Updating Package.swift SwiftPM pins"
 sed -i -r -e "s|(appsflyer-apple-rpc\.git\", exact: \")[0-9]+\.[0-9]+\.[0-9]+|\1${newIosVersion}|g" Package.swift
+sed -i -r -e "s|(AppsFlyerFramework-Static\.git\", exact: \")[0-9]+\.[0-9]+\.[0-9]+|\1${newIosFrameworkVersion}|g" Package.swift
 echo "Updating ios/Podfile CocoaPods pin"
 sed -i -r -e "s/(pod 'AppsFlyerRPC', ')[0-9]+\.[0-9]+\.[0-9]+(')/\1${newIosVersion}\2/g" ios/Podfile
 git add package.json README.md Package.swift ios/Podfile
--- .github/workflows/scripts/updatePlugin.sh
+++ .github/workflows/scripts/updatePlugin.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Check that there are five arguments
-if [ $# -ne 5 ]; then
-  echo "Usage: $0 pluginVersion=<pluginVersion> buildNumber=<buildNumber> iOSVersion=<iOSVersion> androidVersion=<androidVersion> androidPluginBridgeVersion=<androidPluginBridgeVersion>"
+# Check that there are six arguments
+if [ $# -ne 6 ]; then
+  echo "Usage: $0 pluginVersion=<pluginVersion> buildNumber=<buildNumber> iOSVersion=<iOSVersion> iOSFrameworkVersion=<iOSFrameworkVersion> androidVersion=<androidVersion> androidPluginBridgeVersion=<androidPluginBridgeVersion>"
   exit 1
 fi
 
@@ -10,19 +10,21 @@ fi
 pluginVersion=$(echo $1| cut -d'=' -f 2)
 buildNumber=$(echo $2| cut -d'=' -f 2)
 iOSVersion=$(echo $3| cut -d'=' -f 2)
-androidVersion=$(echo $4| cut -d'=' -f 2)
-androidPluginBridgeVersion=$(echo $5| cut -d'=' -f 2)
+iOSFrameworkVersion=$(echo $4| cut -d'=' -f 2)
+androidVersion=$(echo $5| cut -d'=' -f 2)
+androidPluginBridgeVersion=$(echo $6| cut -d'=' -f 2)
 
 echo "pluginVersion: $pluginVersion"
 echo "buildNumber: $buildNumber"
 echo "iOSVersion: $iOSVersion"
+echo "iOSFrameworkVersion: $iOSFrameworkVersion"
 echo "androidVersion: $androidVersion"
 echo "androidPluginBridgeVersion: $androidPluginBridgeVersion"
 
 ./.github/workflows/scripts/updatePluginVersion.sh $pluginVersion
 ./.github/workflows/scripts/updateBuildNumber.sh $buildNumber
-./.github/workflows/scripts/updateIosVersion.sh $iOSVersion
+./.github/workflows/scripts/updateIosVersion.sh $iOSVersion $iOSFrameworkVersion
 ./.github/workflows/scripts/updateAndroidVersion.sh $androidVersion $androidPluginBridgeVersion
 
 # run from project root
-#./.github/workflows/scripts/updatePlugin.sh pluginVersion=6.10.5 buildNumber=120 iOSVersion=6.10.3 androidVersion=6.10.6 androidPluginBridgeVersion=6.10.6
+#./.github/workflows/scripts/updatePlugin.sh pluginVersion=6.10.5 buildNumber=120 iOSVersion=6.10.3 iOSFrameworkVersion=6.10.1 androidVersion=6.10.6 androidPluginBridgeVersion=6.10.6
